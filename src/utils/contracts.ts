9d03cb09fd2f64bed521b6f9010b15e4139d2172
janewise <janewise2001@gmail.com>
2024-09-07 18:07:30 +0630

    feee18

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -7,19 +7,19 @@ const nftContractAddress = "0x37b233040eA24d474619d18eD5B121cae5febd2d";
 const rewardTokenContractAddress = "0x9506A4dCdcDeAFD9796f835FB2a90482E7Aa05Fc";
 const stakingContractAddress = "0xE167D91364b4942a1E463958802280886E33DA87";
 
-export const NFT_CONTRACT = getContract({
+export const NFT_CONTRACT =( getContract as any)({
     client: client,
     chain: chain,
     address: nftContractAddress
 });
 
-export const REWARD_TOKEN_CONTRACT = getContract({
+export const REWARD_TOKEN_CONTRACT =( getContract as any)({
     client: client,
     chain: chain,
     address: rewardTokenContractAddress
 });
 
-export const STAKING_CONTRACT = getContract({
+export const STAKING_CONTRACT =( getContract as any)({
     client: client,
     chain: chain,
     address: stakingContractAddress,
