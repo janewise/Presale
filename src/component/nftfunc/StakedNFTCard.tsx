b41d0d6fdce7032b4097cd790fbc48a3a1fe7c34
janewise <janewise2001@gmail.com>
2024-09-07 15:22:04 +0630

    feee8

--- src/component/nftfunc/StakedNFTCard.tsx
+++ src/component/nftfunc/StakedNFTCard.tsx
@@ -241,6 +241,91 @@
 //     </div>
 //   );
 // };
+
+
+//04
+// import { MediaRenderer, TransactionButton, useReadContract } from "thirdweb/react";
+// import { getNFT } from "thirdweb/extensions/erc721";
+// import { NFT_CONTRACT, STAKING_CONTRACT } from "../../utils/contracts";
+// import { useState } from "react";
+// import { prepareContractCall } from "thirdweb";
+// import { client } from "../../app/client";
+
+// type StakedNFTCardProps = {
+//   tokenId: bigint;
+//   refetchStakedInfo: () => void;
+//   refetchOwnedNFTs: () => void;
+// };
+
+// export const StakedNFTCard = ({ tokenId, refetchStakedInfo, refetchOwnedNFTs }: StakedNFTCardProps) => {
+//   // Use the hook to read the NFT data
+//   const { data: nft, isLoading, error } = useReadContract(getNFT, {
+//     contract: NFT_CONTRACT,
+//     tokenId: tokenId,
+//   });
+
+//   // Handle loading state
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   // Handle error state
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   // State for hover effect
+//   const [isHovered, setIsHovered] = useState(false);
+
+//   return (
+//     <div
+//       style={{ margin: "10px" }}
+//       onMouseEnter={() => setIsHovered(true)}
+//       onMouseLeave={() => setIsHovered(false)}
+//     >
+//       <MediaRenderer
+//         client={client}
+//         src={nft?.metadata.image}
+//         style={{
+//           borderRadius: "10px",
+//           marginBottom: "10px",
+//           height: "200px",
+//           width: "200px",
+//         }}
+//       />
+//       <p style={{ margin: "0 10px 10px 10px" }}>{nft?.metadata.name}</p>
+
+//       <TransactionButton
+//         transaction={() =>
+//           prepareContractCall({
+//             contract: STAKING_CONTRACT,
+//             method: "withdraw",
+//             params: [[tokenId]],
+//           })
+//         }
+//         onTransactionConfirmed={() => {
+//           refetchOwnedNFTs();
+//           refetchStakedInfo();
+//           alert("Withdrawn!");
+//         }}
+//         style={{
+//           border: "none",
+//           color: "#fff",
+//           padding: "10px",
+//           borderRadius: "10px",
+//           cursor: "pointer",
+//           width: "100%",
+//           fontSize: "12px",
+//           backgroundColor: isHovered ? "rgba(255, 255, 255, 0.64)" : "rgba(255, 255, 255, 0.33)",
+//         }}
+//       >
+//         Withdraw
+//       </TransactionButton>
+//     </div>
+//   );
+// };
+
+//05
 import { MediaRenderer, TransactionButton, useReadContract } from "thirdweb/react";
 import { getNFT } from "thirdweb/extensions/erc721";
 import { NFT_CONTRACT, STAKING_CONTRACT } from "../../utils/contracts";
